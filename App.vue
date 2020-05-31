146eac4772b1cea41f554172016a2306c21cea3e
zhulong1999 <zhulong1999@gmail.com>
2020-05-31 22:33:10 +0800

    项目初始化

---
+++ App.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+
+};
+</script>
+
+<style>
+/* 解决头条小程序组件内引入字体不生效的问题 */
+/* #ifdef MP-TOUTIAO */
+@font-face {
+	font-family: uniicons;
+	src: url('/static/uni.ttf');
+}
+/* #endif */
+</style>
